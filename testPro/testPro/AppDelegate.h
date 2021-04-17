57a9cd063154664ccfb90393fbbabac2bf3cb613
wqoeyi0n0514 <zhing12365@163.com>
2021-04-17 14:46:29 +0800

    new

---
+++ testPro/testPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testPro
+//
+//  Created by 恒达的mini on 2017/6/26.
+//  Copyright © 2017年 孙世龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testPro/testPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testPro
+//
+//  Created by 恒达的mini on 2017/6/26.
+//  Copyright © 2017年 孙世龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
