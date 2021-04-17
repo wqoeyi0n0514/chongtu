50ef2cd90a756ba82e87a9a6786db91f3ba4eade
wqoeyi0n0514 <zhing12365@163.com>
2021-04-17 15:29:05 +0800

    fajghk

---
+++ testPro/testPro/AAView.h
@@ -0,0 +1,13 @@
+//
+//  AAView.h
+//  testPro
+//
+//  Created by liuyi on 2017/6/26.
+//  Copyright © 2017年 孙世龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAView : UIView
+
+@end
---
+++ testPro/testPro/AAView.m
@@ -0,0 +1,21 @@
+//
+//  AAView.m
+//  testPro
+//
+//  Created by liuyi on 2017/6/26.
+//  Copyright © 2017年 孙世龙. All rights reserved.
+//
+
+#import "AAView.h"
+
+@implementation AAView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
